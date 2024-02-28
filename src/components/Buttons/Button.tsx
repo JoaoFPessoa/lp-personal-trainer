d0326a7b431a5204c487962f1330739a60cfc2c9
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-28 19:38:01 -0300

    add: button

---
+++ src/components/Buttons/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface ButtonProps {
+  children: React.ReactNode; // Or React.ElementType if you expect a specific type of children
+  href: string;
+}
+
+export default function Button({ children, href }: ButtonProps) {
+  return (
+    <a
+      href={href}
+      className="transform text-sm md:text-base hover:scale-105 transition duration-300"
+    >
+      {children}
+    </a>
+  );
+}
---
+++ src/components/Buttons/ContactButton.tsx
@@ -0,0 +1,11 @@
+export default function QuoteButton() {
+  return (
+    <a
+      href="https://wa.me/5511942558728?text=Ol%C3%A1,%20te%20encontrei%20no%20seu%20site%20e%20gostaria%20de%20esclarecer%20algumas%20d%C3%BAvidas"
+      target="_blank"
+      className="bg-red-600 rounded-xl font-bold text-white p-4 text-sm md:text-base transform hover:scale-105 transition duration-300"
+    >
+      Agendar sessão
+    </a>
+  );
+}
