1677dd5479c13bd3ca46ebd99e6c89985de74173
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-28 19:39:46 -0300

    fix: logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="h-[90px] max-w-[100vw] border md:h-[100px] sticky border-b border-gray-200 bg-grayBackground top-0 left-0 w-full z-50">
       <header className="flex justify-between py-8 md:px-10 md:py-5">
         <div className="hidden md:flex justify-center items-center">
-          <h1 className="text-xl font-bold text-">Coach: Renan Guimarães</h1>
+          <h1 className="text-xl font-bold text-">Renan Guimarães</h1>
         </div>
         <div className="px-2 md:px-0 w-full md:w-[40%] ">
           <Navbar />
