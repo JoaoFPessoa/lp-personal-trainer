b25370a9a5d2df284d71ab9ba4548fe39ac1f042
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-28 20:46:13 -0300

    add: metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,41 @@ import Banner from "@/components/Banner";
 import Services from "@/components/Sections/Services";
 import Testimonials from "@/components/Sections/Testimonials";
 import Contact from "@/components/Sections/Contact";
+import Head from "next/head";
 
 const lexend = Lexend({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={`${lexend.className}`}>
-      <Header />
-      <div id="body" className="px-[5%] md:px-[10%] flex flex-col gap-10 mt-10">
-        <Banner />
-        <Services />
-        <Testimonials />
-        <Contact />
-      </div>
-    </main>
+    <>
+      <Head>
+        {/* Metadata for a personal trainer */}
+        <title>
+          Renan Guimarães: Personal Trainer em Aldeia da Serra, Vila Olímpia,
+          and Moema
+        </title>
+        <meta
+          name="description"
+          content="Personal trainer em Aldeia da Serra, Vila Olímpia, Moema."
+        />
+        <meta
+          name="keywords"
+          content="personal trainer, coach fitness, fitness, Aldeia da Serra, Vila Olímpia, Moema, treinos, planos de treinamento, guia de nutrição, personal trainer certificado, objetivos fitness"
+        />
+        {/* Add more meta tags as needed */}
+      </Head>
+      <main className={`${lexend.className}`}>
+        <Header />
+        <div
+          id="body"
+          className="px-[5%] md:px-[10%] flex flex-col gap-10 mt-10"
+        >
+          <Banner />
+          <Services />
+          <Testimonials />
+          <Contact />
+        </div>
+      </main>
+    </>
   );
 }
