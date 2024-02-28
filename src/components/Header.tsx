82eab3fc9bf0714ed2aa46714f794b2c6b57dc3c
JoaoFPessoa <joaoppessoa@uol.com.br>
2024-02-28 20:08:59 -0300

    styles: add background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Navbar from "./Navbar";
 export default function Header() {
   return (
-    <div className="h-[90px] max-w-[100vw] border md:h-[100px] sticky border-b border-gray-200 bg-grayBackground top-0 left-0 w-full z-50">
+    <div className="h-[90px] bg-white max-w-[100vw] border md:h-[100px] sticky border-b border-gray-200 bg-grayBackground top-0 left-0 w-full z-50">
       <header className="flex justify-between py-8 md:px-10 md:py-5">
         <div className="hidden md:flex justify-center items-center">
           <h1 className="text-xl font-bold text-">Renan Guimarães</h1>
